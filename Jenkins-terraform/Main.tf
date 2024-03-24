17bd2d727a8fcb2aa01f6403c337720ab50370e4
techinanutshellhack <itohan.eregie@techinanutshellhack.com>
2024-03-24 11:11:44 +0000

    new change

--- Jenkins-terraform/Main.tf
+++ Jenkins-terraform/Main.tf
@@ -60,7 +60,7 @@ resource "aws_security_group" "Jenkins-sg" {
 resource "aws_instance" "web" {
   ami                    = "ami-03f4878755434977f"
   instance_type          = "t2.large"
-  key_name               = "Argo key"
+  key_name               = "jenkins"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
   user_data              = templatefile("./install_jenkins.sh", {})
   iam_instance_profile   = aws_iam_instance_profile.example_profile.name
--- Jenkins-terraform/backend.tf
+++ Jenkins-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "mario123bucket" # Replace with your actual S3 bucket name
-    key    = "Jenkins/terraform.tfstate"
-    region = "ap-south-1"
+    bucket = "jenkins-terraform-cicd1" # Replace with your actual S3 bucket name
+    key    = "terraform.tfstate"
+    region = "eu-north-1"
   }
 }
